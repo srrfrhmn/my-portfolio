845c626473d11bb6a76bb434486e05d464526bf8
srrfrhmn <srrfrhmn@gmail.com>
2025-10-23 00:20:07 -0400

    updated dependencies

--- next.config.js
+++ next.config.js
@@ -1,22 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(pdf)$/,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            publicPath: "/_next/static/files",
-            outputPath: "static/files",
-            name: "[name].[ext]",
-          },
-        },
-      ],
-    });
-
-    return config;
+  turbopack: {},  // Enable Turbopack with default settings
+  experimental: {
+    // Handle PDF files in the public directory instead
+    outputFileTracingExcludes: {
+      '/public/**/*.pdf': true,
+    },
   },
 };
 
